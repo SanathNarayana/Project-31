0abbb8f67112ccbbb126d1b1c222c770f3824ad6
SanathNarayana <68953929+SanathNarayana@users.noreply.github.com>
2021-08-05 20:07:47 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,86 +1,114 @@
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
+const Events = Matter.Events;
 const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
 
-var engine, world;
-var box1, pig1,pig3;
-var backgroundImg,platform;
-var bird, slingshot;
-var gameState="onsling";
+var particles = [];
+var plinkos = []; 
+var divisions = [];
+var divisionHeight = 300;
 
-function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+function setup() {
+  createCanvas(800, 700);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width / 2, height, width, 20);
+
+  //create division objects
+  for (var j = 0; j <= width; j=j + 80) {
+    divisions.push(
+      new Divisions(j, height - divisionHeight / 3, 10, divisionHeight)
+    );
+  }
+  for (var j = 0; j<= 160; j=j + 160) {
+    divisions.push(
+      new Divisions(j, height - divisionHeight / 3, 10, divisionHeight)
+    );
+  }
+  for (var j= 0; j <= 240; j=j + 240) {
+    divisions.push(
+      new Divisions(j, height - divisionHeight / 3, 10, divisionHeight)
+    );
+  }
+  for (var j = 0; j <= 320; j=j + 320) {
+    divisions.push(
+      new Divisions(j, height - divisionHeight / 3, 10, divisionHeight)
+    );
+  }
+  for (var j = 0; j<= 400; j = j + 400) {
+    divisions.push(
+      new Divisions(j, height - divisionHeight / 3, 10, divisionHeight)
+    );
+  }
+  for (var j = 0; j<= 480; j=j + 480) {
+    divisions.push(
+      new Divisions(j, height - divisionHeight / 3, 10, divisionHeight)
+    );
+  }
+  for (var j = 0; j <= 560; j=j + 560) {
+    divisions.push(
+      new Divisions(j, height - divisionHeight / 3, 10, divisionHeight)
+    );
+  }
+  for (var j= 0; j <= 640; j=j + 640) {
+    divisions.push(
+      new Divisions(j, height - divisionHeight / 3, 10, divisionHeight)
+    );
+  }
+  for (var j= 0; j<= 720; j = j + 720) {
+    divisions.push(
+      new Divisions(j, height - divisionHeight / 3, 10, divisionHeight)
+    );
+  }
+  for (var j = 0; j <= 800; j = j + 800) {
+    divisions.push(
+      new Divisions(j, height - divisionHeight / 3, 10, divisionHeight)
+    );
+  }
+
+  //create 1st row of plinko objects
+  for (var i = 75; i <= width; i=i + 50) {
+    plinkos.push(new Plinko(i, 75));
+  }
+
+  //create 2nd row of plinko objects
+  for (var i = 50; i<= width - 10; i= i + 50) {
+    plinkos.push(new Plinko(i, 175));
+  }
+
+  //create 3rd row of plinko objects
+  for (var i = 40; i <= width; i = i + 50) {
+    plinkos.push(new Plinko(i, 275));
+  }
+
+  //create 4th row of plinko objects
+  for (var i = 15; i <= width - 10; i = i + 50) {
+    plinkos.push(new Plinko(i, 375));
+  }
 }
 
-function setup(){
-    var canvas = createCanvas(1200,400);
-    engine = Engine.create();
-    world = engine.world;
+function draw() {
+  Engine.update(engine);
+  rectMode(CENTER);
+  background("orange");
 
+  ground.display();
 
-    ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+  //display the plinkos
+  for (var i = 0; i < plinkos.length; i++) {
+    plinkos[i].display();
+  }
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
+  //display the divisions
+  for (var j = 0; j< divisions.length; j++) {
+    divisions[j].display();
+  }
 
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
-
-    log3 =  new Log(810,180,300, PI/2);
-
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
-
-    bird = new Bird(200,50);
-
-    //log6 = new Log(230,180,80, PI/2);
-    slingshot = new SlingShot(bird.body,{x:200, y:50});
-}
-
-function draw(){
-    background(backgroundImg);
-    Engine.update(engine);
-    //strokeWeight(4);
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
-
-    box3.display();
-    box4.display();
-    pig3.display();
-    log3.display();
-
-    box5.display();
-    log4.display();
-    log5.display();
-
-    bird.display();
-    platform.display();
-    //log6.display();
-    slingshot.display();    
-}
-
-function mouseDragged(){
-    if(gameState!=="launched"){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
-}
-}
-
-function mouseReleased(){
-    slingshot.fly();
-    gameState="launched";
-}
-
-function keyPressed(){
-    if(keyCode === 32){
-    //    slingshot.attach(bird.body);
-    }
+  //particles
+  if (frameCount %60===0) {
+    particles.push(new Particle(random(0,800),0,8));
+  }
+  for (var k = 0; k < particles.length; k++) {
+    particles[k].display();
+  }
 }
